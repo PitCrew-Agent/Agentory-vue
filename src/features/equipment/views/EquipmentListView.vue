075387ce56cd0546cea579d1294f5ceaa5d8470a
shiro <no4h1209@gmail.com>
2026-07-06 16:27:27 +0900

    feat: 설비 목록 라인 선택과 3D 오버레이 구현

---
+++ src/features/equipment/views/EquipmentListView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DashboardFramePage from '@/features/dashboard/components/DashboardFramePage.vue'
+import EquipmentListPanel from '@/features/equipment/components/EquipmentListPanel.vue'
+import { equipmentListGroups } from '@/features/equipment/mock/equipmentListMock'
+</script>
+
+<template>
+  <DashboardFramePage active-navigation-id="equipment" content-label="설비 목록 영역">
+    <EquipmentListPanel :groups="equipmentListGroups" />
+  </DashboardFramePage>
+</template>
